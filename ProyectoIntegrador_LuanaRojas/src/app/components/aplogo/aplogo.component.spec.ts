022e49af97dc772b1df17f2df8b07f91c027000a
lrojas0 <109438990+luana0r@users.noreply.github.com>
2022-07-19 03:21:23 -0300

    Agregue los logos e imagenes para el header.

---
+++ ProyectoIntegrador_LuanaRojas/src/app/components/aplogo/aplogo.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { APlogoComponent } from './aplogo.component';
+
+describe('APlogoComponent', () => {
+  let component: APlogoComponent;
+  let fixture: ComponentFixture<APlogoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ APlogoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(APlogoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
